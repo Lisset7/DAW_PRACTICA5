1454d7e85c8708efab8004b294194f25e35be5ec
Lisset7 <lis98onti@gmail.com>
2025-04-16 20:39:06 -0600

    Add files via upload

---
+++ JAVA SCRIPT/foto-list.js
@@ -0,0 +1,15 @@
+const PHOTO_LIST= [
+    'images/images/conejo.jpg',
+    'images/images/gato.jpg',
+    'images/images/lobo.jpg',
+    'images/images/mono.jpg',
+    'images/images/panda.jpg',
+    'images/images/perro.jpeg',
+    'images/images/suricata.jpg',
+    'images/images/tigre.jpg',
+    'images/images/zorro.jpg',
+
+
+
+
+];
